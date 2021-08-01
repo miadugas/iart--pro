9878b8450a4391d6a3e5e2395615521527b0630a
Mia <miamorticia@Mias-MBP.hsd1.co.comcast.net>
2021-08-01 15:20:07 -0600

    frontend template - bootstrap responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from'react';
-
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 const App = () => {
   return (
     <>
+      <Header />
+    <main>
+      <Container>
       <h1> Welcome to Time Furlow Fine Art - Pro</h1>
+      </Container>
+    </main>
+      <Footer />
     </>
   );
 }
